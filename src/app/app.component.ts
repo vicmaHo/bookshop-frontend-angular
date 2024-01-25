1e4b8d1dacf34fab7f7e97d7eeeccda5039bb070
Victor Hernandez <victormanu968@gmail.com>
2024-01-25 16:53:35 -0500

    Configuracion inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatSidenavModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bookshop-web';
+  title = 'BookShop - App';
 }
